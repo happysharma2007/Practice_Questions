db1cc00f63d6fac80b3abff9e85c4bd6cca1d0ac
happysharma2007 <happysharma112007@gmail.com>
2024-10-25 20:20:51 +0530

    Add files via upload

---
+++ avraage.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a , b , average;
+cout<< " enter the value of a & b = ";
+cin >> a >> b;
+average = (a+b)/2;
+cout<<"the average is = " << average;
+return 0;
+
+}
